6bc5a41cd0752bed80c18ce16e9103c2b07f82fe
Aneeq321 <156693713+Aneeq321@users.noreply.github.com>
2024-01-15 23:08:39 +0500

    Add files via upload

---
+++ genetics.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package exampractice;
+
+/**
+ * @author A N E E Q
+ *
+ */
+public class genetics {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
